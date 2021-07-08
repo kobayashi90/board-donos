2863fe438d58316679a33bcfe32e0ac4b874892e
jorgev259 <jorgev259@outlook.es>
2021-07-07 18:48:12 -0600

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
-function App() {
+import info from './info.json'
+const { goalText, goalNumber, currentNumber } = info
+
+function App () {
+  const percent = currentNumber / goalNumber * 100
+  const percentLeft = 100 - percent
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid'>
+      <div className='row ann py-4'>
+        <div className='col mx-3'>
+          <div className='row pb-3'>
+            <div className='col goal'>Donation Goal ({goalText}): {currentNumber}$ / {goalNumber}$</div>
+          </div>
+          <div className='row'>
+            <div className='col'>
+              <div className='progress'>
+                <div className='progress-bar progress-bar-striped progress-bar-animated bg-success' role='progressbar' aria-valuenow={percent} aria-valuemin='0' aria-valuemax='100' style={{ width: `${percent}%` }} />
+                <div className='progress-bar progress-bar-striped progress-bar-animated bg-danger' role='progressbar' aria-valuenow={percentLeft} aria-valuemin='0' aria-valuemax='100' style={{ width: `${percentLeft}%` }} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
